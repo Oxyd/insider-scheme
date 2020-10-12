e5df15e08938841b28afd435fd70123c34a75c44
Ondřej Majerech <majerech.o@gmail.com>
2020-10-12 02:10:54 +0200

    Move (insider internal) into its own file

---
+++ src/internal_module.hpp
@@ -0,0 +1,13 @@
+#ifndef INSIDER_INTERNAL_MODULE_HPP
+#define INSIDER_INTERNAL_MODULE_HPP
+
+#include "scheme.hpp"
+
+namespace insider {
+
+module
+make_internal_module(context&);
+
+} // namespace insider
+
+#endif
