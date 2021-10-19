ac36338e29bf2efeb017dfafbcab85bf68d00b37
Ondřej Majerech <majerech.o@gmail.com>
2021-10-19 20:45:45 +0200

    case-lambda

---
+++ lib/scheme/case-lambda.scm
@@ -0,0 +1,25 @@
+(library (scheme case-lambda))
+(import (insider base-scheme) (insider syntax) (insider error) (insider control))
+(export case-lambda)
+
+(define-syntax case-lambda
+  (syntax-rules ()
+    ((case-lambda (params0 body0 ...) ...)
+     (lambda args
+       (let ((len (length args)))
+         (letrec-syntax
+             ((clause (syntax-rules --- ()
+                        ((clause)
+                         (error "case-lambda: No matching clause"))
+
+                        ((clause ((params ---) . body) . rest)
+                         (if (= len (length '(params ---)))
+                             (apply (lambda (params ---) . body) args)
+                             (clause . rest)))
+
+                        ((clause ((params --- . tail) . body) . rest)
+                         (if (>= len (length '(params ---)))
+                             (apply (lambda (params --- . tail) . body) args)
+                             (clause . rest))))))
+
+           (clause (params0 body0 ...) ...)))))))
