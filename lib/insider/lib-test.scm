f436677dae2691757970318a9d9e18246ffe57fd
Ondřej Majerech <majerech.o@gmail.com>
2022-01-07 22:41:54 +0100

    Add tests to lib-test

--- lib/insider/lib-test.scm
+++ lib/insider/lib-test.scm
@@ -1,11 +1,16 @@
 (library (insider lib-test))
 (import (insider syntax) (insider control) (insider test)
-        (insider basic-procedures-test) (insider syntax-rules-test) (insider syntax-test))
+        (insider basic-procedures-test) (insider numeric-test)
+        (insider opt-lambda-test) (insider string-test)
+        (insider syntax-rules-test) (insider syntax-test))
 (export test-lib)
 
 (define (test-lib)
   (test-group "library"
     (test-basic-procedures)
+    (test-numeric)
+    (test-opt-lambda)
+    (test-string)
     (test-syntax-transformers)
     (test-syntax)))
 
