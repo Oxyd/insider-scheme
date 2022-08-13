547f42c0e82f61f219cbdbebfd896671bfd1dbc6
Ondřej Majerech <majerech.o@gmail.com>
2022-08-13 19:09:05 +0200

    define_struct helper

---
+++ src/util/define_struct.hpp
@@ -0,0 +1,155 @@
+#ifndef INSIDER_UTIL_DEFINE_STRUCT_HPP
+#define INSIDER_UTIL_DEFINE_STRUCT_HPP
+
+#include "context.hpp"
+#include "module.hpp"
+#include "util/define_procedure.hpp"
+
+#include <fmt/format.h>
+
+#include <string>
+#include <type_traits>
+
+namespace insider {
+
+template <typename T>
+class struct_definer {
+public:
+  struct_definer(context& ctx, std::string name, ptr<insider::module_> m)
+    : ctx_{ctx}
+    , name_{std::move(name)}
+    , module_{m}
+  {
+    define_procedure<&struct_definer::predicate>(
+      ctx, make_predicate_name(), m
+    );
+  }
+
+  template <auto Member>
+  requires std::is_member_object_pointer_v<decltype(Member)>
+  struct_definer&
+  field(std::string const& name) {
+    define_procedure<object_getter<Member>>(
+      ctx_, make_getter_name(name), module_
+    );
+    return *this;
+  }
+
+  template <auto Member>
+  requires std::is_member_object_pointer_v<decltype(Member)>
+  struct_definer&
+  mutable_field(std::string const& name) {
+    define_procedure<object_getter<Member>>(
+      ctx_, make_getter_name(name), module_
+    );
+    define_procedure<object_setter<Member>>(
+      ctx_, make_setter_name(name), module_
+    );
+    return *this;
+  }
+
+  template <auto Getter>
+  requires std::is_member_function_pointer_v<decltype(Getter)>
+  struct_definer&
+  field(std::string const& name) {
+    define_procedure<function_getter<Getter>>(
+      ctx_, make_getter_name(name), module_
+    );
+    return *this;
+  }
+
+  template <auto Getter, auto Setter>
+  requires std::is_member_function_pointer_v<decltype(Getter)>
+           && std::is_member_function_pointer_v<decltype(Setter)>
+  struct_definer&
+  mutable_field(std::string const& name) {
+    define_procedure<function_getter<Getter>>(
+      ctx_, make_getter_name(name), module_
+    );
+    define_procedure<function_setter<Setter>>(
+      ctx_, make_setter_name(name), module_
+    );
+    return *this;
+  }
+
+private:
+  template <typename>
+  struct member_type;
+
+  template <typename U>
+  struct member_type<U T::*> {
+    using type = U;
+  };
+
+  template <typename U>
+  using member_type_t = typename member_type<U>::type;
+
+  template <typename>
+  struct setter_type;
+
+  template <typename U>
+  struct setter_type<void (T::*)(U)> {
+    using type = U;
+  };
+
+  template <typename U>
+  using setter_type_t = typename setter_type<U>::type;
+
+  context&              ctx_;
+  std::string           name_;
+  ptr<insider::module_> module_;
+
+  static bool
+  predicate(ptr<> x) {
+    return is<T>(x);
+  }
+
+  template <auto Member>
+  static auto
+  object_getter(ptr<T> x) {
+    return x.value()->*Member;
+  }
+
+  template <auto Member>
+  static void
+  object_setter(ptr<T> x, member_type_t<decltype(Member)> value) {
+    x.value()->*Member = std::move(value);
+  }
+
+  template <auto Getter>
+  static auto
+  function_getter(ptr<T> x) {
+    return (x.value()->*Getter)();
+  }
+
+  template <auto Setter>
+  static void
+  function_setter(ptr<T> x, setter_type_t<decltype(Setter)> value) {
+    (x.value()->*Setter)(std::move(value));
+  }
+
+  std::string
+  make_predicate_name() {
+    return fmt::format("{}?", name_);
+  }
+
+  std::string
+  make_getter_name(std::string const& field_name) {
+    return fmt::format("{}-{}", name_, field_name);
+  }
+
+  std::string
+  make_setter_name(std::string const& field_name) {
+    return fmt::format("{}-{}-set!", name_, field_name);
+  }
+};
+
+template <typename T>
+struct_definer<T>
+define_struct(context& ctx, std::string name, ptr<insider::module_> m) {
+  return struct_definer<T>{ctx, std::move(name), m};
+}
+
+} // namespace insider
+
+#endif
---
+++ test/test_define_struct.cpp
@@ -0,0 +1,79 @@
+#include "scheme_fixture.hpp"
+
+#include "util/define_struct.hpp"
+
+using namespace insider;
+
+struct define_struct_fixture : scheme_fixture { };
+
+struct s : leaf_object<s> {
+  static constexpr char const* scheme_name = "insider::s";
+
+  int number;
+
+  explicit
+  s(int n) : number{n} { }
+
+  int
+  get_number() const { return number; }
+
+  void
+  set_number(int value) { number = value; }
+};
+
+TEST_F(define_struct_fixture, defines_predicate) {
+  define_struct<s>(ctx, "s", ctx.internal_module());
+  auto f = eval("s?");
+  EXPECT_EQ(call_with_continuation_barrier(ctx, f, {make<s>(ctx, 0)}).get(),
+            ctx.constants->t);
+  EXPECT_EQ(call_with_continuation_barrier(ctx, f, {ctx.intern("s")}).get(),
+            ctx.constants->f);
+}
+
+TEST_F(define_struct_fixture, accessor_for_member) {
+  define_struct<s>(ctx, "s", ctx.internal_module())
+    .field<&s::number>("number")
+    ;
+  auto f = eval("s-number");
+  EXPECT_EQ(
+    expect<integer>(
+      call_with_continuation_barrier(ctx, f, {make<s>(ctx, 12)}).get()
+    ).value(),
+    12
+  );
+}
+
+TEST_F(define_struct_fixture, accessor_for_getter) {
+  define_struct<s>(ctx, "s", ctx.internal_module())
+    .field<&s::get_number>("number")
+    ;
+  auto f = eval("s-number");
+  EXPECT_EQ(
+    expect<integer>(
+      call_with_continuation_barrier(ctx, f, {make<s>(ctx, 12)}).get()
+    ).value(),
+    12
+  );
+}
+
+TEST_F(define_struct_fixture, setter_for_member) {
+  define_struct<s>(ctx, "s", ctx.internal_module())
+    .mutable_field<&s::number>("number")
+    ;
+  auto f = eval("(lambda (x) (s-number-set! x (* (s-number x) 2)) x)");
+  auto value = expect<s>(
+    call_with_continuation_barrier(ctx, f, {make<s>(ctx, 6)}).get()
+  );
+  EXPECT_EQ(value->number, 12);
+}
+
+TEST_F(define_struct_fixture, setter_for_setter) {
+  define_struct<s>(ctx, "s", ctx.internal_module())
+    .mutable_field<&s::get_number, &s::set_number>("number")
+    ;
+  auto f = eval("(lambda (x) (s-number-set! x (* (s-number x) 2)) x)");
+  auto value = expect<s>(
+    call_with_continuation_barrier(ctx, f, {make<s>(ctx, 6)}).get()
+  );
+  EXPECT_EQ(value->number, 12);
+}
