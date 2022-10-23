0ee4273ed0248e5108d27e96d1212daa38660808
Ondřej Majerech <majerech.o@gmail.com>
2022-10-23 18:59:18 +0200

    Have constant evaluator keep its own environment

--- src/compiler/evaluate_constants_pass.cpp
+++ src/compiler/evaluate_constants_pass.cpp
@@ -5,7 +5,76 @@
 #include "runtime/basic_types.hpp"
 #include "vm/vm.hpp"
 
+#include <stdexcept>
+#include <unordered_map>
+
 namespace insider {
+
+namespace {
+  struct variable_info {
+    variable   var;
+    expression init_expr;
+
+    explicit
+    variable_info(ptr<local_variable> var)
+      : var{var}
+      , init_expr{var->constant_initialiser()}
+    { }
+
+    explicit
+    variable_info(ptr<top_level_variable> var)
+      : var{var}
+      , init_expr{var->constant_initialiser()}
+    { }
+  };
+
+  class static_environment {
+  public:
+    ~static_environment() noexcept {
+      assert(scopes_.empty());
+    }
+
+    void
+    push_scope() {
+      scopes_.emplace_back();
+    }
+
+    void
+    pop_scope() {
+      assert(!scopes_.empty());
+      scopes_.pop_back();
+    }
+
+    variable_info&
+    add_variable(ptr<local_variable> var) {
+      return scopes_.back().emplace_back(variable_info{var});
+    }
+
+    variable_info&
+    find_variable(ptr<local_variable> v) {
+      for (auto& scope : scopes_ | std::views::reverse)
+        for (auto& info : scope)
+          if (info.var == v)
+            return info;
+
+      assert(false);
+      throw std::logic_error{"Unknown variable"};
+    }
+
+    variable_info
+    find_variable(ptr<local_variable> var) const {
+      return const_cast<static_environment*>(this)->find_variable(var);
+    }
+
+    variable_info
+    find_variable(ptr<top_level_variable> var) const {
+      return variable_info{var};
+    }
+
+  private:
+    std::vector<std::vector<variable_info>> scopes_;
+  };
+}
 
 static bool
 is_reference_to_constant_variable(expression e) {
@@ -22,55 +91,57 @@ is_constant_propagable_expression(expression e) {
   return is<literal_expression>(e) || is_reference_to_constant_variable(e);
 }
 
-static bool
-is_constant_propagable(auto var) {
-  return var->constant_initialiser()
-         && is_constant_propagable_expression(var->constant_initialiser());
-}
+static ptr<>
+constant_value_for_expression(expression e, static_environment const& env);
 
 static ptr<>
-constant_value_for_expression(expression e);
-
-static ptr<>
-constant_value_for_expression(ptr<literal_expression> lit) {
+constant_value_for_expression(ptr<literal_expression> lit,
+                              static_environment const&) {
   return lit->value();
 }
 
 static ptr<>
-constant_value_for_expression(ptr<local_reference_expression> ref) {
-  if (expression k = ref->variable()->constant_initialiser())
-    if (auto lit = match<literal_expression>(k))
+constant_value_for_expression(ptr<local_reference_expression> ref,
+                              static_environment const& env) {
+  auto info = env.find_variable(ref->variable());
+  if (info.init_expr)
+    if (auto lit = match<literal_expression>(info.init_expr))
       return lit->value();
   return {};
 }
 
 static ptr<>
-constant_value_for_expression(ptr<top_level_reference_expression> ref) {
-  if (expression k = ref->variable()->constant_initialiser())
-    if (auto lit = match<literal_expression>(k))
+constant_value_for_expression(ptr<top_level_reference_expression> ref,
+                              static_environment const& env) {
+  auto info = env.find_variable(ref->variable());
+  if (info.init_expr)
+    if (auto lit = match<literal_expression>(info.init_expr))
       return lit->value();
   return {};
 }
 
 static ptr<>
-constant_value_for_expression(ptr<sequence_expression> seq) {
+constant_value_for_expression(ptr<sequence_expression> seq,
+                              static_environment const& env) {
   // Technically the correct condition is "all subexpressions are constant",
   // but it's quite unlikely that a sequence with multiple subexpressions will
   // be all-constant.
 
   if (seq->expressions().size() == 1)
-    return constant_value_for_expression(seq->expressions().front());
+    return constant_value_for_expression(seq->expressions().front(), env);
   else
     return {};
 }
 
 static ptr<>
-constant_value_for_expression(auto) { return {}; }
+constant_value_for_expression(auto, static_environment const&) { return {}; }
 
 static ptr<>
-constant_value_for_expression(expression e) {
-  return visit([] (auto expr) { return constant_value_for_expression(expr); },
-               e);
+constant_value_for_expression(expression e, static_environment const& env) {
+  return visit(
+    [&] (auto expr) { return constant_value_for_expression(expr, env); },
+    e
+  );
 }
 
 static expression
@@ -96,13 +167,14 @@ find_constant_evaluable_callable(context& ctx,
 }
 
 static bool
-can_be_constant_evaluated(ptr<application_expression> app) {
+can_be_constant_evaluated(ptr<application_expression> app,
+                          static_environment const& env) {
   return std::ranges::all_of(
     app->arguments(),
-    [] (expression e) {
+    [&] (expression e) {
       return visit(
-        [] (auto expr) {
-          return constant_value_for_expression(expr) != nullptr;
+        [&] (auto expr) {
+          return constant_value_for_expression(expr, env) != nullptr;
         },
         e
       );
@@ -129,11 +201,12 @@ coalesce_eqv_values(context& ctx, std::vector<ptr<>>& values) {
 
 static std::vector<ptr<>>
 make_arguments_for_constant_evaluation(context& ctx,
-                                       ptr<application_expression> app) {
+                                       ptr<application_expression> app,
+                                       static_environment const& env) {
   std::vector<ptr<>> result;
   result.reserve(app->arguments().size());
   for (expression arg : app->arguments())
-    result.push_back(constant_value_for_expression(arg));
+    result.push_back(constant_value_for_expression(arg, env));
 
   coalesce_eqv_values(ctx, result);
   return result;
@@ -141,10 +214,11 @@ make_arguments_for_constant_evaluation(context& ctx,
 
 static expression
 evaluate_constant_application(context& ctx, ptr<> callable,
-                              ptr<application_expression> app) {
+                              ptr<application_expression> app,
+                              static_environment const& env) {
   try {
     ptr<> result = call_with_continuation_barrier(
-      ctx, callable, make_arguments_for_constant_evaluation(ctx, app)
+      ctx, callable, make_arguments_for_constant_evaluation(ctx, app, env)
     );
     return make<literal_expression>(ctx, result);
   } catch (...) {
@@ -167,8 +241,9 @@ namespace {
       node(expression e) : expr{e} { }
     };
 
-    context&     ctx;
-    result_stack results;
+    context&           ctx;
+    result_stack       results;
+    static_environment env;
 
     explicit
     evaluate_constants_visitor(context& ctx)
@@ -188,6 +263,17 @@ namespace {
     }
 
     void
+    enter_expression(ptr<lambda_expression> lambda, node&,
+                     dfs_stack<node>& stack) {
+      env.push_scope();
+      for (auto p : lambda->parameters())
+        env.add_variable(p);
+      env.add_variable(lambda->self_variable());
+
+      push_children(lambda, stack);
+    }
+
+    void
     enter_expression(ptr<let_expression> let, node& n, dfs_stack<node>& stack) {
       // Process definition expressions first so that constant bindings for the
       // introduced variables are known when processing the body.
@@ -199,9 +285,20 @@ namespace {
 
     void
     enter_expression(auto expr, node&, dfs_stack<node>& stack) {
+      push_children(expr, stack);
+    }
+
+    void
+    push_children(auto expr, dfs_stack<node>& stack) {
       expr->visit_subexpressions([&] (expression child) {
         stack.push_back({child});
       });
+    }
+
+    bool
+    leave_expression(ptr<lambda_expression> lambda, node&, dfs_stack<node>&) {
+      env.pop_scope();
+      return leave_expression_for_final_time(lambda);
     }
 
     bool
@@ -212,8 +309,10 @@ namespace {
         update_let_bound_variables(let);
         stack.push_back({let->body()});
         return false;
-      } else
+      } else {
+        env.pop_scope();
         return leave_expression_for_final_time(let);
+      }
     }
 
     bool
@@ -230,50 +329,55 @@ namespace {
 
     void
     update_let_bound_variables(ptr<let_expression> let) {
+      env.push_scope();
+
       auto expressions = results
         | std::views::drop(results.size() - let->definitions().size());
 
       for (std::size_t i = 0; i < let->definitions().size(); ++i) {
         auto var = let->definitions()[i].variable();
         auto expr = expressions[i];
-        if ((constant_value_for_expression(expr)
+        auto& info = env.add_variable(var);
+
+        if ((constant_value_for_expression(expr, env)
              || is_constant_propagable_expression(expr))
             && !var->flags().is_set
             && !var->flags().is_loop_variable)
-          var->set_constant_initialiser(
-            ctx.store, constant_initialiser_expression(expr)
-          );
+          info.init_expr = constant_initialiser_expression(expr);
       }
     }
 
     expression
     combine(ptr<top_level_reference_expression> ref) {
-      if (is_constant_propagable(ref->variable()))
-        return ref->variable()->constant_initialiser();
+      auto info = env.find_variable(ref->variable());
+      if (info.init_expr && is_constant_propagable_expression(info.init_expr))
+        return info.init_expr;
       else
         return ref;
     }
 
     expression
     combine(ptr<local_reference_expression> ref) {
-      if (is_constant_propagable(ref->variable()))
-        return ref->variable()->constant_initialiser();
+      auto info = env.find_variable(ref->variable());
+      if (info.init_expr && is_constant_propagable_expression(info.init_expr))
+        return info.init_expr;
       else
         return ref;
     }
 
     expression
     combine(ptr<application_expression> app) {
-      if (can_be_constant_evaluated(app))
+      if (can_be_constant_evaluated(app, env))
         if (auto callable = find_constant_evaluable_callable(ctx, app))
-          if (auto result = evaluate_constant_application(ctx, callable, app))
+          if (auto result = evaluate_constant_application(ctx, callable, app,
+                                                          env))
             return result;
       return app;
     }
 
     expression
     combine(ptr<if_expression> ifexpr) {
-      if (auto test_value = constant_value_for_expression(ifexpr->test())) {
+      if (auto test_value = constant_value_for_expression(ifexpr->test(), env)) {
         if (test_value == ctx.constants->f)
           return ifexpr->alternative();
         else
