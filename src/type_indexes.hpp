b29ea1a2c6a3a855406070d16e7feb940499dff8
Ondřej Majerech <majerech.o@gmail.com>
2022-11-14 18:00:14 +0100

    Formatting

--- src/type_indexes.hpp
+++ src/type_indexes.hpp
@@ -1,41 +1,37 @@
 #ifndef INSIDER_TYPE_INDEXES_HPP
 #define INSIDER_TYPE_INDEXES_HPP
 
-namespace insider {
+namespace insider::type_indexes {
 
-namespace type_indexes {
-  // Plain enum for implicit convertibility to word_type.
+// Plain enum for implicit convertibility to word_type.
+enum index {
+  null = 1,
+  void_,
+  eof,
+  parameter_tag,
+  core_form,
+  tail_call_tag,
+  boolean,
+  pair,
+  vector,
+  bytevector,
+  box,
+  procedure_prototype,
+  procedure,
+  native_procedure,
+  values_tuple,
+  big_integer,
+  fraction,
+  floating_point,
+  complex,
+  record_type,
+  record_instance,
+  string,
+  symbol,
+  syntax,
+  scope
+};
 
-  enum index {
-    null = 1,
-    void_,
-    eof,
-    parameter_tag,
-    core_form,
-    tail_call_tag,
-    boolean,
-    pair,
-    vector,
-    bytevector,
-    box,
-    procedure_prototype,
-    procedure,
-    native_procedure,
-    values_tuple,
-    big_integer,
-    fraction,
-    floating_point,
-    complex,
-    record_type,
-    record_instance,
-    string,
-    symbol,
-    syntax,
-    scope
-  };
-
-} // namespace type_index
-
-} // namespace insider
+} // namespace insider::type_indexes
 
 #endif
