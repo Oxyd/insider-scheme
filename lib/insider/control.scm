16ebf9602fc3f3811cc545e686a22f82fa7cb153
Ondřej Majerech <majerech.o@gmail.com>
2023-01-01 17:44:38 +0100

    Export call-with-continuation-barrier from (insider control)

--- lib/insider/control.scm
+++ lib/insider/control.scm
@@ -9,12 +9,14 @@
               values call-with-values with-exception-handler raise
               raise-continuable dynamic-wind main-module?-tag environment
               interactive-environment eval meta current-expand-module-tag
-              dynamic-import interaction-environment-specifier-tag))
+              dynamic-import interaction-environment-specifier-tag
+              call-with-continuation-barrier))
 (export call-with-current-continuation call/cc let/cc make-parameter
         make-parameter-from-tag parameterize apply values call-with-values
         with-exception-handler raise raise-continuable dynamic-wind guard
         when-main-module environment interactive-environment eval meta
-        current-expand-module dynamic-import interaction-environment-specifier)
+        current-expand-module dynamic-import interaction-environment-specifier
+        call-with-continuation-barrier)
 
 (define (call-with-current-continuation f)
   (capture-stack
